9148b97880c9faa74679b3de71d5239d69d74689
Bevin Benson <bevin@fb.com>
2018-07-17 16:44:44 -0700

    Integrated map and results

--- social-maps/View Controllers/SearchResultsViewController.m
+++ social-maps/View Controllers/SearchResultsViewController.m
@@ -21,7 +21,23 @@
     GMSAutocompleteResultsViewController *_resultsViewController;
     UISearchController *_searchController;
 }
+
 - (void)viewDidLoad {
+    
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.86
+                                                            longitude:151.20
+                                                                 zoom:6];
+    GMSMapView *mapView = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+    mapView.myLocationEnabled = YES;
+    
+    self.view = mapView;    
+    
+    // Creates a marker in the center of the map.
+    GMSMarker *marker = [[GMSMarker alloc] init];
+    marker.position = CLLocationCoordinate2DMake(-33.86, 151.20);
+    marker.title = @"Sydney";
+    marker.snippet = @"Australia";
+    marker.map = mapView;
     
     _resultsViewController = [[GMSAutocompleteResultsViewController alloc] init];
     _resultsViewController.delegate = self;
