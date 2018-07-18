c35569978177b364db88ae5f54fca0fb736e4dee
César Francisco Barraza <cbarrazaa1@fb.com>
2018-07-18 16:57:54 -0700

    Profile viewcontroller looks better

--- social-maps/View Controllers/ProfileViewController.m
+++ social-maps/View Controllers/ProfileViewController.m
@@ -15,15 +15,26 @@
 #import "PFUser+ExtendedUser.h"
 
 @interface ProfileViewController () <CLLocationManagerDelegate, GMSMapViewDelegate>
-@property (weak, nonatomic) IBOutlet UIView *profileView;
+// Outlet Definitions //
+@property (weak, nonatomic) IBOutlet UIView *profileMapView;
+@property (weak, nonatomic) IBOutlet UIImageView *profilePicture;
+@property (weak, nonatomic) IBOutlet UILabel *displayNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *handleLabel;
+@property (weak, nonatomic) IBOutlet UIButton *friendsButton;
+@property (weak, nonatomic) IBOutlet UILabel *hometownLabel;
+@property (weak, nonatomic) IBOutlet UILabel *bioLabel;
 
+@property (weak, nonatomic) IBOutlet UIView *profilePictureView;
+@property (weak, nonatomic) IBOutlet UIView *myPlacesView;
+
+
+// Instance Properties //
 @property (strong, nonatomic) CLLocationManager* locationManager;
 @property (strong, nonatomic) CLLocation* currentLocation;
 @property (strong, nonatomic) GMSMapView* mapView;
 @property (strong, nonatomic) NSArray<GMSPlace*>* favorites;
 @property (strong, nonatomic) NSArray<GMSPlace*>* wishlist;
 @property (strong, nonatomic) NSMutableDictionary<NSString*, GMSPlace*>* markers;
-
 
 
 // TEMPORARY
@@ -48,15 +59,40 @@
     
     // create map
     GMSCameraPosition* camera = [GMSCameraPosition cameraWithLatitude:0 longitude:0 zoom:6];
-    self.mapView = [GMSMapView mapWithFrame:self.profileView.bounds camera:camera];
+    self.mapView = [GMSMapView mapWithFrame:self.profileMapView.bounds camera:camera];
     self.mapView.settings.myLocationButton = YES;
     [self.mapView setMyLocationEnabled:YES];
-    [self.profileView addSubview:self.mapView];
+    [self.profileMapView addSubview:self.mapView];
     self.mapView.delegate = self;
     
     // get favs and wishlist
     self.markers = [NSMutableDictionary new];
     [self retrieveUserPlaces];
+    
+    [self makeUILookGood];
+}
+
+- (void)makeUILookGood {
+    [self makeRoundCorners:self.profilePicture];
+    [self makeRoundCorners:self.profilePictureView];
+    [self addShadow:self.profilePictureView withOffset:CGSizeZero];
+    self.friendsButton.layer.cornerRadius = 18;
+    self.friendsButton.clipsToBounds = YES;
+    [self addShadow:self.friendsButton withOffset:CGSizeMake(0, 4)];
+    [self addShadow:self.myPlacesView withOffset:CGSizeMake(0,6)];
+}
+
+- (void)makeRoundCorners:(UIView*)view {
+    view.layer.cornerRadius = view.frame.size.width / 2;
+    view.clipsToBounds = YES;
+}
+
+- (void)addShadow:(UIView*)view withOffset:(CGSize)offset {
+    view.layer.shadowColor = [UIColor blackColor].CGColor;
+    view.layer.shadowOffset = offset;
+    view.layer.shadowRadius = 5;
+    view.layer.shadowOpacity = 0.2;
+    view.layer.masksToBounds = NO;
 }
 
 - (void)retrieveUserPlaces {
