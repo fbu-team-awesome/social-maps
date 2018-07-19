aae45cc8e4e82e8ec1fb718d5c1d9e2dee8ce8bd
César Francisco Barraza <cbarrazaa1@fb.com>
2018-07-19 12:58:01 -0700

    Can now pick profile picture from camera/gallery and added a Helper class.

---
+++ social-maps/Helper.h
@@ -0,0 +1,20 @@
+//
+//  Helper.h
+//  social-maps
+//
+//  Created by César Francisco Barraza on 7/19/18.
+//  Copyright © 2018 Bevin Benson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+
+@interface Helper : NSObject
++ (void)showAlertWithTitle:(NSString*)title message:(NSString*)message sender:(UIViewController*)sender;
++ (void)showPhotoAlert:(UIViewController*)sender;
++ (void)setImageFromPFFile:(PFFile*)file forImageView:(UIImageView*)imageView;
++ (void)setImageFromPFFile:(PFFile*)file forButton:(UIButton*)button;
++ (UIImage*)resizeImage:(UIImage*)image withSize:(CGSize)size;
++ (PFFile*)getPFFileFromImage:(UIImage*)image;
+@end
