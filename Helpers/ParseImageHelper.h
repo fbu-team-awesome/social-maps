1aa61f74baf44a5a57f5510936f70751b3b349b5
César Francisco Barraza <cbarrazaa1@fb.com>
2018-07-20 13:46:57 -0700

    Moved parse-image (PFFIle) related helper methods into its own helper class.

--- Helpers/ParseImageHelper.h
+++ Helpers/ParseImageHelper.h
@@ -1,21 +1,15 @@
 //
-//  Helper.h
+//  ParseImageHelper.h
 //  social-maps
 //
-//  Created by César Francisco Barraza on 7/19/18.
+//  Created by César Francisco Barraza on 7/20/18.
 //  Copyright © 2018 Bevin Benson. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
 #import <Parse/Parse.h>
 
-@interface Helper : NSObject
-+ (void)showAlertWithTitle:(NSString*)title message:(NSString*)message sender:(UIViewController*)sender;
-+ (void)showPhotoAlertFrom:(UIViewController*)sender;
-
-+ (UIImage*)resizeImage:(UIImage*)image withSize:(CGSize)size;
-
+@interface ParseImageHelper : NSObject
 + (void)setImageFromPFFile:(PFFile*)file forImageView:(UIImageView*)imageView;
 + (void)setImageFromPFFile:(PFFile*)file forButton:(UIButton*)button;
 + (PFFile*)getPFFileFromImage:(UIImage*)image;
