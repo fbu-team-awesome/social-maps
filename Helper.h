f0df611fdf18fa808cca8d35d34821e273ca2368
César Francisco Barraza <cbarrazaa1@fb.com>
2018-07-20 13:44:46 -0700

    Moved image-related helper methods into its own helper class.

--- Helper.h
+++ Helper.h
@@ -13,6 +13,8 @@
 @interface Helper : NSObject
 + (void)showAlertWithTitle:(NSString*)title message:(NSString*)message sender:(UIViewController*)sender;
 + (void)showPhotoAlertFrom:(UIViewController*)sender;
+
+
 + (void)setImageFromPFFile:(PFFile*)file forImageView:(UIImageView*)imageView;
 + (void)setImageFromPFFile:(PFFile*)file forButton:(UIButton*)button;
 + (UIImage*)resizeImage:(UIImage*)image withSize:(CGSize)size;
