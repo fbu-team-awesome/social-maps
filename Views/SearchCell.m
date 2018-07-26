b33ac799f09136d26f4ff04d82a2ef2f9dde823e
César Francisco Barraza <cbarrazaa1@fb.com>
2018-07-26 15:38:32 -0700

    You can now unfavorite from the map searchresults tableview.

--- Views/SearchCell.m
+++ Views/SearchCell.m
@@ -8,6 +8,7 @@
 
 #import "SearchCell.h"
 #import "PFUser+ExtendedUser.h"
+#import "NCHelper.h"
 
 @interface SearchCell ()
 @property (weak, nonatomic) IBOutlet UIButton *favoriteButton;
@@ -21,36 +22,68 @@
 
 - (void)awakeFromNib {
     [super awakeFromNib];
-    // Initialization code
-    
 }
 
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
-}
 - (void) configureCell {
     if (_prediction) {
         _nameLabel.text =  _prediction.attributedPrimaryText.string;
         _addressLabel.text = _prediction.attributedSecondaryText.string;
+        
+        [Place checkPlaceWithIDExists:_prediction.placeID
+                               result:^(Place * _Nonnull result)
+         {
+             self.parsePlace = result;
+             
+             // get Google place
+             [[APIManager shared] GMSPlaceFromPlace:self.parsePlace
+                                     withCompletion:^(GMSPlace *place)
+              {
+                  self.place = place;
+              }];
+             
+             // update button states
+             [self.favoriteButton setSelected:[[PFUser currentUser].favorites containsObject:self.parsePlace]];
+         }];
     } else {
         NSLog(@"No place found for cell");
     }
 }
-- (IBAction)didTapFavorite:(id)sender {
-    //TODO: add condition for unfavoriting
-    [Place checkPlaceWithIDExists:_prediction.placeID result:^(Place * result) {
-        [[APIManager shared] GMSPlaceFromPlace:result
-                             withCompletion:^(GMSPlace *place)
-                             {
-                                 [[PFUser currentUser] addFavorite:place];
-                                 [[NSNotificationCenter defaultCenter] postNotificationName:@"AddFavoriteNotification" object:place];
-                             }
-         ];
-    }];
 
+- (IBAction)didTapFavorite:(id)sender {
+    // if we have no place yet, do not do anything
+    if(self.place == nil)
+    {
+        return;
+    }
+    
+    // check if it is favorited or not
+    if(self.favoriteButton.selected)
+    {
+        [[PFUser currentUser] removeFavorite:self.place];
+        [NCHelper notify:NTRemoveFavorite object:self.place];
+    }
+    else
+    {
+        [[PFUser currentUser] addFavorite:self.place];
+        [NCHelper notify:NTAddFavorite object:self.place];
+    }
+    
+    // animate
+    [UIView animateWithDuration:0.1 animations:^{
+        self.favoriteButton.transform = CGAffineTransformMakeScale(1.25, 1.25);
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:0.1 animations:^{
+            self.favoriteButton.transform = CGAffineTransformIdentity;
+        }];
+    }];
+    
+    // haptic feedback
+    [[UIImpactFeedbackGenerator new] impactOccurred];
+    
+    // set the state
+    [self.favoriteButton setSelected:!self.favoriteButton.selected];
 }
+
 - (IBAction)didTapWishlist:(id)sender {
     [Place checkPlaceWithIDExists:_prediction.placeID result:^(Place * result) {
         [[APIManager shared] GMSPlaceFromPlace:result
