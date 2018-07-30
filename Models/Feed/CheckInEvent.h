b6576c9883241be581343472993511c8434bd827
César Francisco Barraza <cbarrazaa1@fb.com>
2018-07-30 15:08:26 -0700

    Created CheckIn feed event type model.

---
+++ Models/Feed/CheckInEvent.h
@@ -0,0 +1,14 @@
+//
+//  CheckInEvent.h
+//  social-maps
+//
+//  Created by César Francisco Barraza on 7/30/18.
+//  Copyright © 2018 Bevin Benson. All rights reserved.
+//
+
+#import "FeedEvent.h"
+
+@interface CheckInEvent : FeedEvent
+@property (strong, nonatomic, nullable) NSArray<PFFile *> *photos;
+@property (strong, nonatomic, nullable) Review *review;
+@end
