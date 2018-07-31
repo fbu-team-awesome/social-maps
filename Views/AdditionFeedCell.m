5adeab8fc5d3bc28db4583340c68edb06ec0c474
César Francisco Barraza <cbarrazaa1@fb.com>
2018-07-30 18:05:28 -0700

    Added files for Feed VC and its tableview.

---
+++ Views/AdditionFeedCell.m
@@ -0,0 +1,24 @@
+//
+//  AdditionFeedCell.m
+//  social-maps
+//
+//  Created by César Francisco Barraza on 7/30/18.
+//  Copyright © 2018 Bevin Benson. All rights reserved.
+//
+
+#import "AdditionFeedCell.h"
+
+@implementation AdditionFeedCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
