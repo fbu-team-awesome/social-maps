368909e2ef8947fa2946a847e7e90f2203df60f0
César Francisco Barraza <cbarrazaa1@fb.com>
2018-08-01 14:10:37 -0700

    Created a review addition subclass of FeedEvent.

---
+++ Models/Feed/ReviewAdditionEvent.m
@@ -0,0 +1,23 @@
+//
+//  ReviewAdditionEvent.m
+//  social-maps
+//
+//  Created by César Francisco Barraza on 8/1/18.
+//  Copyright © 2018 Bevin Benson. All rights reserved.
+//
+
+#import "ReviewAdditionEvent.h"
+
+@implementation ReviewAdditionEvent
+- (instancetype)initWithParseObject:(PFObject *)object {
+    self = [super initWithParseObject:object];
+    self.review = object[@"review"];
+    
+    return self;
+}
+
+- (void)setParseProperties {
+    [super setParseProperties];
+    self.parseObject[@"review"] = self.review;
+}
+@end
