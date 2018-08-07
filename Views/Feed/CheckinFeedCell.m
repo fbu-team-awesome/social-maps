ea9a14a758f8135b3d2160a65e67c4f442a756b2
César Francisco Barraza <cbarrazaa1@fb.com>
2018-08-07 14:57:36 -0700

    Added UIStyling to each of the feed cells (code is the same for all of them)

--- Views/Feed/CheckinFeedCell.m
+++ Views/Feed/CheckinFeedCell.m
@@ -8,9 +8,11 @@
 
 #import "CheckinFeedCell.h"
 #import "ParseImageHelper.h"
+#import "UIStylesHelper.h"
 
 @interface CheckinFeedCell ()
 @property (weak, nonatomic) IBOutlet UIImageView *profilePictureImage;
+@property (weak, nonatomic) IBOutlet UIView *pictureView;
 @property (weak, nonatomic) IBOutlet UILabel *contentLabel;
 @property (weak, nonatomic) IBOutlet UILabel *timeLabel;
 @property (strong, nonatomic) CheckInEvent *event;
@@ -27,16 +29,24 @@
 
 - (void)initUI {
     // set up content formatting
-    NSString *content = [NSString stringWithFormat:@"%@ just checked-in at '%@'!", self.event.user.displayName, self.event.place.placeName];
+    NSString *content = [NSString stringWithFormat:@"%@ checked in to %@.", self.event.user.displayName, self.event.place.placeName];
+    UIFont *font = [UIFont fontWithName:@"AvenirNext-DemiBold" size:13];
+    
+    NSMutableAttributedString *attributedContent = [[NSMutableAttributedString alloc] initWithString:content];
+    [attributedContent beginEditing];
+    [attributedContent addAttribute:NSFontAttributeName value:font range:NSMakeRange(0, self.event.user.displayName.length)];
+    [attributedContent addAttribute:NSFontAttributeName value:font range:NSMakeRange(self.event.user.displayName.length + 15, self.event.place.placeName.length)];
+    [attributedContent endEditing];
     
     // update UI
-    self.contentLabel.text = content;
+    [self.contentLabel setAttributedText:[attributedContent copy]];
     [ParseImageHelper setImageFromPFFile:self.event.user.profilePicture forImageView:self.profilePictureImage];
     self.timeLabel.text = [self.event getTimpestamp];
     
     // set rounded image
-    self.profilePictureImage.layer.cornerRadius = self.profilePictureImage.frame.size.width / 2;
-    self.profilePictureImage.clipsToBounds = YES;
+    [UIStylesHelper addRoundedCornersToView:self.pictureView];
+    [UIStylesHelper addRoundedCornersToView:self.profilePictureImage];
+    [UIStylesHelper addShadowToView:self.pictureView];
     
     // fade into visibility
     [UIView animateWithDuration:0.3 animations:^{
--- Views/Feed/ReviewFeedCell.m
+++ Views/Feed/ReviewFeedCell.m
@@ -8,9 +8,11 @@
 
 #import "ReviewFeedCell.h"
 #import "ParseImageHelper.h"
+#import "UIStylesHelper.h"
 
 @interface ReviewFeedCell ()
 @property (weak, nonatomic) IBOutlet UIImageView *profilePictureImage;
+@property (weak, nonatomic) IBOutlet UIView *pictureView;
 @property (weak, nonatomic) IBOutlet UILabel *contentLabel;
 @property (weak, nonatomic) IBOutlet UILabel *ratingLabel;
 @property (weak, nonatomic) IBOutlet UILabel *reviewLabel;
@@ -28,18 +30,26 @@
 
 - (void)initUI {
     // set up content formatting
-    NSString *content = [NSString stringWithFormat:@"%@ added a picture for '%@'.", self.event.user.displayName, self.event.place.placeName];
+    NSString *content = [NSString stringWithFormat:@"%@ added a review for %@.", self.event.user.displayName, self.event.place.placeName];
+    UIFont *font = [UIFont fontWithName:@"AvenirNext-DemiBold" size:13];
+    
+    NSMutableAttributedString *attributedContent = [[NSMutableAttributedString alloc] initWithString:content];
+    [attributedContent beginEditing];
+    [attributedContent addAttribute:NSFontAttributeName value:font range:NSMakeRange(0, self.event.user.displayName.length)];
+    [attributedContent addAttribute:NSFontAttributeName value:font range:NSMakeRange(self.event.user.displayName.length + 20, self.event.place.placeName.length)];
+    [attributedContent endEditing];
     
     // update UI
-    self.contentLabel.text = content;
+    [self.contentLabel setAttributedText:[attributedContent copy]];
     self.ratingLabel.text = [NSString stringWithFormat:@"%i/5", self.event.review.rating];
     self.reviewLabel.text = self.event.review.content;
     [ParseImageHelper setImageFromPFFile:self.event.user.profilePicture forImageView:self.profilePictureImage];
     self.timeLabel.text = [self.event getTimpestamp];
     
     // set rounded image
-    self.profilePictureImage.layer.cornerRadius = self.profilePictureImage.frame.size.width / 2;
-    self.profilePictureImage.clipsToBounds = YES;
+    [UIStylesHelper addRoundedCornersToView:self.pictureView];
+    [UIStylesHelper addRoundedCornersToView:self.profilePictureImage];
+    [UIStylesHelper addShadowToView:self.pictureView];
     
     // fade into visibility
     [UIView animateWithDuration:0.3 animations:^{
